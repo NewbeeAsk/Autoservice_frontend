bec8e225cdf0e38468938abfbee9f5c3a79b06bf
NewbeeAsk <romannosadchuk@gmail.com>
2021-01-20 11:42:02 +0200

    fix remarks

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,6 +10,6 @@ export class APIService {
   constructor(private http: HttpClient) { }
 
   getServices(): Observable<any> {
-    return this.http.get(`http://localhost:8080/auto-service/service`);
+    return this.http.get(`http://localhost:8080/auto-service`);
   }
 }
