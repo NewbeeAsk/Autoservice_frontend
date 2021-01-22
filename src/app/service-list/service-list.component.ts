6bbd09795bc96486a1d0a3e8fb71641bc5ae6df5
NewbeeAsk <romannosadchuk@gmail.com>
2021-01-22 17:05:11 +0200

    implement filtration by categories

--- src/app/service-list/service-list.component.ts
+++ src/app/service-list/service-list.component.ts
@@ -1,23 +1,38 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 import Service from '../service.model';
+import {AsyncAction} from 'rxjs/internal/scheduler/AsyncAction';
 
 @Component({
   selector: 'app-service-list',
   templateUrl: './service-list.component.html',
   styleUrls: ['./service-list.component.scss']
 })
-export class ServiceListComponent implements OnInit {
+export class ServiceListComponent implements OnChanges {
 
   @Input() public serviceList!: Service[];
+  categoryServiceList!: Service[];
   @Output() addServiceToCart = new EventEmitter<Service>();
+  SelectedValue: string;
 
   constructor() {
+    this.SelectedValue = 'all';
+    this.categoryServiceList = [];
   }
 
-  ngOnInit(): void {
+  ngOnChanges(): any {
+    this.categoryServiceList = this.serviceList;
   }
 
   addToCart(service: Service): any {
     this.addServiceToCart.emit(service);
+  }
+
+  searchByCategory(): any {
+    if (this.SelectedValue === 'all') {
+      this.categoryServiceList = this.serviceList;
+    } else {
+      this.categoryServiceList = this.serviceList
+        .filter(el => el.category === this.SelectedValue);
+    }
   }
 }
